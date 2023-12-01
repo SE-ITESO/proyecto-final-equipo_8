72574041119a68279ebbf95fcb61ae5b52ae5472
Brandon_Guzman <gutierrez.brandon.18.0246@gmail.com>
2023-11-30 21:41:40 -0600

    Juego insano v4

--- tablero.h
+++ tablero.h
@@ -33,7 +33,7 @@ typedef enum{
 void tablero_init(void);
 
 /*!
- * @brief Function to accommodate the arrau
+ * @brief Function to accommodate the array
  */
 void tablero_acomodo_arreglo(void);
 
@@ -42,17 +42,58 @@ void tablero_acomodo_arreglo(void);
  */
 void tablero_print_fichas(void);
 
+/*!
+ * @brief Function to switch a element in a specific array
+ *
+ * @param uint8_t* array
+ * @param uint8_t old_character
+ * @param uint8_t new_character
+ */
 void tablero_switch_string(uint8_t* array, uint8_t old_character, uint8_t new_character);
+
+/*!
+ * @brief Function to realize a movement
+ *
+ * @param uint8_t x_old
+ * @param uint8_t y_old
+ * @param uint8_t x_new
+ * @param uint8_t y_new
+ */
 void tablero_movimiento(uint8_t x_old, uint8_t y_old, uint8_t x_new, uint8_t y_new);
 
+/*!
+ * @brief Function to control the tablero
+ *
+ * @param uint8_t* jugador
+ * @param uint8_t* array_button
+ * @param uint8_t* reinicio
+ *
+ * @retval uint8_t status of the game
+ */
 uint8_t tablero_control(uint8_t* jugador, uint8_t* array_button, uint8_t* reinicio);
 
+/*!
+ * @brief Function to print tablero with chess pieces
+ */
 void tablero_print_tablero(void);
 
+/*!
+ * @brief Function to assign a log number
+ *
+ * @param uint8_t number
+ */
 void tablero_assign_log_number(uint8_t number);
 
+/*!
+ * @brief Function to init a replay
+ *
+ * @param uint8_t log_number
+ */
 void tablero_repeticion_init(uint8_t log_number);
-void tablero_avanza_movimiento();
-void tablero_retrocede_movimiento();
+
+/*!
+ * @brief Function to move forward in movements
+ */
+void tablero_avanza_movimiento(void);
 
 #endif /* TABLERO_H_ */
